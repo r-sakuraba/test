0233fdb30e7b5433814e4bb398bef5c338371fe0
r-sakuraba <ruki.bloom@gmail.com>
2019-05-30 23:48:29 +0900

    App 設定.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DeckSampleComponent } from './deck-sample/deck-sample.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+   { path: 'deck-sample', component: DeckSampleComponent }
+ ];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, DeckSampleComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
