050fa7bf68b0299452b4b941928057e3b6b99afe
r-sakuraba <ruki.bloom@gmail.com>
2019-05-31 01:27:23 +0900

    設定変更.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DeckSampleComponent } from './deck-sample/deck-sample.component';
 import { RouterModule, Routes } from '@angular/router';
+import {FormsModule} from "@angular/forms";
 
 const routes: Routes = [
-   { path: 'deck-sample', component: DeckSampleComponent }
- ];
+  { path: 'deck-sample', component: DeckSampleComponent },
+];
 
 @NgModule({
   declarations: [AppComponent, DeckSampleComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes), FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
